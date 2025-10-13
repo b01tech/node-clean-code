f33fcadfe64a80545b42e8cca16f7fa1834129c2
b01tech <bruno01.tech@gmail.com>
2025-10-13 20:08:52 -0300

    fix(signup): handle unexpected errors in signup controller

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,6 @@
 import { InvalidParamsError } from "../error/invalid-params-error";
 import { MissingParamsError } from "../error/missing-params-error";
+import { ServerError } from "../error/server-error";
 import { badRequest } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validator";
@@ -8,15 +9,23 @@ import { httpRequest, httpResponse } from "../protocols/http";
 export class SignupController implements Controller {
   constructor(private readonly emailValidator: EmailValidator) {}
   handle(request: httpRequest): httpResponse {
-    const requiredParams = ["name", "email", "password", "confirmPassword"];
-    for (const param of requiredParams) {
-      if (!request.body[param]) {
-        return badRequest(new MissingParamsError(param));
+    try {
+      const requiredParams = ["name", "email", "password", "confirmPassword"];
+      for (const param of requiredParams) {
+        if (!request.body[param]) {
+          return badRequest(new MissingParamsError(param));
+        }
       }
-    }
-    const isValidEmail = this.emailValidator.isValid(request.body.email);
-    if (!isValidEmail) {
-      return badRequest(new InvalidParamsError("email"));
+      const isValidEmail = this.emailValidator.isValid(request.body.email);
+      if (!isValidEmail) {
+        return badRequest(new InvalidParamsError("email"));
+      }
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      return {
+        status: 500,
+        body: new ServerError(),
+      };
     }
     return {
       status: 200,
